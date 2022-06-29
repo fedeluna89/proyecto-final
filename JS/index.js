d34eb811b2195fb5ef7ab35e10275de3d5e99723
Federico Luna <fedelunacjs@gmail.com>
2022-06-28 21:47:20 -0300

    se agrega la pestaña tienda

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+const productos = [
+    {nombre: "instituto", precio: 7000},
+    {nombre: "denver", precio: 176000},
+    {nombre: "parque", precio: 5000},
+    {nombre: "atenas", precio: 6000},
+    ];
+
+    let filtro = productos.filter(elemento => elemento.precio < 6000);
+    console.log(filtro);
+
+    let dato = prompt("ingresa el nombre del producto a consultar").toLowerCase();
+
+    let articulo = productos.find(elemento => elemento.nombre === dato);
+    alert(`el precio del prodicto ${dato} es ${articulo.precio}`);
+
+
+    
