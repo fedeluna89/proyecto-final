dc35cf59cdb28d6d53b5abf210393c12c4f22cbf
Federico Luna <fedelunacjs@gmail.com>
2022-07-09 22:28:23 -0300

    se agregan imagenes a la tienda

---
+++ JS/contacto.js
@@ -0,0 +1,7 @@
+const respuestaClick = () => {
+    alert("tus datos se enviaron correctamente");
+}
+
+let botonEnviar = document.getElementById("enviar2");
+
+botonEnviar.addEventListener("enviar2", respuestaClick);
--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,29 @@
-const productos = [
-    {nombre: "instituto", precio: 7000},
-    {nombre: "denver", precio: 176000},
-    {nombre: "parque", precio: 5000},
-    {nombre: "atenas", precio: 6000},
-    ];
+const productos = [];
 
-    let filtro = productos.filter(elemento => elemento.precio < 6000);
-    console.log(filtro);
+productos.push(
+    {id: "instituto", nombre: "institutoCamiseta", precio: 7000},
+    {id: "instituto", nombre: "institutoCampera", precio: 9700},
+    {id: "denver", nombre: "denver", precio: 176000},
+    {id: "parque", nombre: "parque", precio: 5000},
+    {id: "atenas", nombre: "atenas", precio: 6000},
+)
+console.log(productos);
 
     let dato = prompt("ingresa el nombre del equipo que te interes").toLowerCase();
 
     let articulo = productos.find(elemento => elemento.nombre === dato);
     alert(`la camiseta oficial de ${dato} tiene el valor de ${articulo.precio} pesos`);
+    /* let contenedor = document.getElementById("contenedor");
+    let dato = prompt("ingresa el nombre del equipo que te interes").toLowerCase();
 
+    let filtrados = productos.filter(elemento => elemento.id === dato);
+
+    for(const producto of filtrados){
+        let item = document.createElement("div");
+        item.innerHTML =   `<h5>id: ${producto.id}
+                            <p>nombre: ${producto.nombre}</p>
+                            <b>precio: ${producto.precio}</b>`
+
+        contenedor.append(item);
+    } */
     alert("gracias por tu consulta");
-
-    
-
